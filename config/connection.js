7c34fb0109ac72b9f652ea811f039a45339367ee
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-04-09 14:56:15 -0400

    moved to env var

--- config/connection.js
+++ config/connection.js
@@ -2,22 +2,21 @@
 var mysql = require("mysql");
 let connection;
 
-if(process.env.JAWSDB_URL){
-    console.log("successfully connected");
-    connection = mysql.createConnection("mysql://z12qrhm2fviv4k15:w7hnr3cwwv3ldtxg@cdm1s48crk8itlnr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ninuwev1w2bf2fcj");
-}else{
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: "Mickey19",
-        database: "cat_db"
-      });
+if (process.env.JAWSDB_URL) {
+  console.log("successfully connected");
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Mickey19",
+    database: "cat_db",
+  });
 }
 
-
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
