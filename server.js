292d4947bf7292d3debd31c8892f74efd1003e86
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-04-09 14:47:01 -0400

    added config and using inside a route

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+const connection = require("./config/connection");
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
@@ -22,12 +24,17 @@ app.set("view engine", "handlebars");
 
 // app.use(routes);
 
-app.get("/", function(req, res) {
-    res.render("index");
-})
+app.get("/", function (req, res) {
+  connection.query("SELECT * FROM cats", function (err, data) {
+    if (err) {
+      return res.status(500).send("An error occurred");
+    }
+    res.render("index", { cats: data });
+  });
+});
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
